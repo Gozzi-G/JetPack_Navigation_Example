5625da8593cb041f288989aabe51d618a5d739e7
Yury Prudnikov <y.prudnikov@growapp.biz>
2022-04-18 16:38:58 +0300

    add dashboard in logic

--- app/src/main/java/com/prud/jetpack_navigation_example/screens/main/tabs/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/prud/jetpack_navigation_example/screens/main/tabs/dashboard/DashboardFragment.kt
@@ -2,16 +2,73 @@ package com.prud.jetpack_navigation_example.screens.main.tabs.dashboard
 
 import android.os.Bundle
 import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.prud.jetpack_navigation_example.R
+import com.prud.jetpack_navigation_example.Repositories
+import com.prud.jetpack_navigation_example.data.model.boxes.entities.Box
 import com.prud.jetpack_navigation_example.databinding.FragmentDashboardBinding
+import com.prud.jetpack_navigation_example.utils.viewModelCreator
+import com.prud.jetpack_navigation_example.views.DashboardItemView
 
 class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
 
     private lateinit var binding: FragmentDashboardBinding
 
+    private val viewModel by viewModelCreator { DashboardViewModel(Repositories.boxesRepository) }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentDashboardBinding.bind(view)
+
+        clearBoxViews()
+        viewModel.boxes.observe(viewLifecycleOwner) { renderBoxes(it) }
+    }
+
+    private fun renderBoxes(boxes: List<Box>) {
+        clearBoxViews()
+        if (boxes.isEmpty()) {
+            binding.noBoxesTextView.visibility = View.VISIBLE
+            binding.boxesContainer.visibility = View.INVISIBLE
+        } else {
+            binding.noBoxesTextView.visibility = View.INVISIBLE
+            binding.boxesContainer.visibility = View.VISIBLE
+            createBoxes(boxes)
+        }
+    }
+
+    private fun createBoxes(boxes: List<Box>) {
+
+        // let's create boxes here by using dynamic view generation
+
+        val width = resources.getDimensionPixelSize(R.dimen.dashboard_item_width)
+        val height = resources.getDimensionPixelSize(R.dimen.dashboard_item_height)
+        val generatedIdentifiers = boxes.map { box ->
+            val id = View.generateViewId()
+            val dashboardItemView = DashboardItemView(requireContext())
+            dashboardItemView.setBox(box)
+            dashboardItemView.id = id
+            dashboardItemView.tag = box
+            dashboardItemView.setOnClickListener(boxClickListener)
+            val params = ConstraintLayout.LayoutParams(width, height)
+            binding.boxesContainer.addView(dashboardItemView, params)
+            return@map id
+        }.toIntArray()
+        binding.flowView.referencedIds = generatedIdentifiers
+    }
+
+    private fun clearBoxViews() {
+        binding.boxesContainer.removeViews(1, binding.root.childCount - 1)
+    }
+
+    private val boxClickListener = View.OnClickListener {
+        val box = it.tag as Box
+        val direction = DashboardFragmentDirections.actionDashboardFragmentToBoxFragment(
+            box.id,
+            getString(box.colorNameRes),
+            box.colorValue
+        )
+        findNavController().navigate(direction)
     }
 }
--- app/src/main/java/com/prud/jetpack_navigation_example/screens/main/tabs/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/prud/jetpack_navigation_example/screens/main/tabs/dashboard/DashboardViewModel.kt
@@ -1,4 +1,27 @@
 package com.prud.jetpack_navigation_example.screens.main.tabs.dashboard
 
-class DashboardViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.prud.jetpack_navigation_example.data.model.boxes.BoxesRepository
+import com.prud.jetpack_navigation_example.data.model.boxes.entities.Box
+import com.prud.jetpack_navigation_example.utils.share
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+class DashboardViewModel(
+    private val boxesRepository: BoxesRepository
+): ViewModel() {
+
+    private val _boxes = MutableLiveData<List<Box>>()
+    val boxes = _boxes.share()
+
+    init {
+        viewModelScope.launch {
+            boxesRepository.getBoxes(onlyActive = true).collect {
+                _boxes.value = it
+            }
+        }
+    }
+
 }
