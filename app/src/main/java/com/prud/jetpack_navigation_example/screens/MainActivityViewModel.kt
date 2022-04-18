2661aa6a2f83601af4fa7360adc2a447de08a062
Yury Prudnikov <y.prudnikov@growapp.biz>
2022-04-18 17:21:39 +0300

    add MainActivity logic

--- app/src/main/java/com/prud/jetpack_navigation_example/screens/MainActivityViewModel.kt
+++ app/src/main/java/com/prud/jetpack_navigation_example/screens/MainActivityViewModel.kt
@@ -1,4 +1,29 @@
 package com.prud.jetpack_navigation_example.screens
 
-class MainActivityViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.prud.jetpack_navigation_example.data.model.accounts.AccountsRepository
+import com.prud.jetpack_navigation_example.utils.share
+import kotlinx.coroutines.launch
+
+class MainActivityViewModel(
+    private val accountsRepository: AccountsRepository
+) : ViewModel() {
+
+    private val _username = MutableLiveData<String>()
+    val username = _username.share()
+
+    init {
+        viewModelScope.launch {
+            // listening for the current account and send the username to be displayed in the toolbar
+            accountsRepository.getAccount().collect {
+                if (it == null) {
+                    _username.value = ""
+                } else {
+                    _username.value = "@${it.userName}"
+                }
+            }
+        }
+    }
 }
