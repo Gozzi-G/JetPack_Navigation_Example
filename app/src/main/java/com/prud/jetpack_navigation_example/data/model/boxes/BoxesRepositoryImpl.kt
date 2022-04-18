f5224c828d1689a52395ec98a96dcdc6ca7ad833
Yury Prudnikov <y.prudnikov@growapp.biz>
2022-04-18 16:36:28 +0300

    add take box logic

--- app/src/main/java/com/prud/jetpack_navigation_example/data/model/boxes/BoxesRepositoryImpl.kt
+++ app/src/main/java/com/prud/jetpack_navigation_example/data/model/boxes/BoxesRepositoryImpl.kt
@@ -1,16 +1,41 @@
 package com.prud.jetpack_navigation_example.data.model.boxes
 
+import android.graphics.Color
+import com.prud.jetpack_navigation_example.R
 import com.prud.jetpack_navigation_example.data.model.accounts.AccountsRepository
 import com.prud.jetpack_navigation_example.data.model.boxes.entities.Box
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.*
 
 
 class BoxesRepositoryImpl(
     private val accountsRepository: AccountsRepository
 ) : BoxesRepository {
 
-    override fun getBoxes(onlyActive: Boolean): Flow<List<Box>> {
-        TODO("Not yet implemented")
+    private val boxes = listOf(
+        Box(1, R.string.green, Color.rgb(0, 128, 0)),
+        Box(2, R.string.red, Color.rgb(128, 0, 0)),
+        Box(3, R.string.blue, Color.rgb(0, 0, 128)),
+        Box(4, R.string.yellow, Color.rgb(128, 128, 0)),
+        Box(5, R.string.black, Color.rgb(0, 0, 0)),
+        Box(6, R.string.violet, Color.rgb(128, 0, 255))
+    )
+
+    private val allActiveBoxes: MutableMap<String, MutableSet<Int>> = mutableMapOf()
+    private val reconstructFlow = MutableSharedFlow<Unit>(replay = 1).also { it.tryEmit(Unit) }
+
+    private val activeBoxesFlow: Flow<Set<Int>> = combine(reconstructFlow, accountsRepository.getAccount()) { _, account ->
+        if (account == null) return@combine emptySet<Int>()
+        val activeIds = allActiveBoxes[account.email] ?: let {
+            val newActiveIdsSet = mutableSetOf<Int>()
+            newActiveIdsSet.addAll(boxes.map { it.id })
+            allActiveBoxes[account.email] = newActiveIdsSet
+            newActiveIdsSet
+        }
+        return@combine HashSet(activeIds)
+    }
+
+    override fun getBoxes(onlyActive: Boolean): Flow<List<Box>> = activeBoxesFlow.map { activeIdentifiers ->
+        boxes.filter { if (onlyActive) activeIdentifiers.contains(it.id) else true }
     }
 
     override suspend fun activateBox(box: Box) {
--- app/src/main/java/com/prud/jetpack_navigation_example/screens/main/tabs/dashboard/BoxViewModel.kt
+++ app/src/main/java/com/prud/jetpack_navigation_example/screens/main/tabs/dashboard/BoxViewModel.kt
@@ -1,4 +1,29 @@
 package com.prud.jetpack_navigation_example.screens.main.tabs.dashboard
 
-class BoxViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.prud.jetpack_navigation_example.data.model.boxes.BoxesRepository
+import com.prud.jetpack_navigation_example.utils.MutableLiveEvent
+import com.prud.jetpack_navigation_example.utils.publishEvent
+import com.prud.jetpack_navigation_example.utils.share
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+
+class BoxViewModel(
+    private val boxId: Int,
+    private val boxesRepository: BoxesRepository
+): ViewModel() {
+
+    private val _shouldExitEvent = MutableLiveEvent<Boolean>()
+    val shouldExitEvent = _shouldExitEvent.share()
+
+    init {
+        viewModelScope.launch {
+            boxesRepository.getBoxes(onlyActive = true)
+                .map { boxes -> boxes.firstOrNull { it.id == boxId } }
+                .collect { currentBox ->
+                    _shouldExitEvent.publishEvent(currentBox == null)
+                }
+        }
+    }
 }
